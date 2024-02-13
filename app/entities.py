a28a1b8aed839365b2c3367441bbec5f2ef04c34
Monica2495 <monrpadilla@misena.edu.co>
2024-02-13 14:14:29 -0600

    proyecto creado

---
+++ app/entities.py
@@ -0,0 +1,28 @@
+class Carro():
+
+    def __init__(self,
+                 placa,
+                 tipo_vehiculo):
+            
+        self.placa = placa
+        self.tipo_vehiculo = tipo_vehiculo
+
+
+class Cliente():
+
+    def __init__(self,
+                 nombre,
+                 celular,
+                 identificacion,
+                 lista_carros):
+        self.nombre = nombre
+        self.celular = celular
+        self.identificacion = identificacion
+        self.lista_carros = lista_carros
+
+    def addCar (self, car):
+        self.lista_carros.append(car)
+
+    def listCar(self):
+        for i in self.lista_carros:
+            print("carro con placas:" + i.placa )
